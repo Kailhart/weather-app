ef92a3e2401d8341802b996ac9f85e0e8fc1be7d
Peresta Kateryna <peresta.kateryna@gmail.com>
2023-09-25 00:14:34 +0200

    linked to git

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        <a href="https://github.com/Kailhart/weather-app" target="_blank">
+          Open-sourced code
+        </a>{" "}
+        by Kateryna Peresta
+      </footer>
+    </div>
+  );
 }
